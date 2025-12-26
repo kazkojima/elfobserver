cac7897592753a4d23fdaa2dddeec30eaea8d742
Kaz Kojima <kkojima@rr.iij4u.or.jp>
2025-12-26 12:42:45 +0900

    src/qpdshow.py: Handle options.

--- src/qpdshow.py
+++ src/qpdshow.py
@@ -8,14 +8,35 @@ if len(sys.argv) < 2:
     print('error: h5 file name required')
     exit()
 
+h5file = None
+title_string = None
+cmap = 'plasma'
+for i in range(1,len(sys.argv)):
+    argstr = sys.argv[i]
+    if argstr.startswith('--title='):
+        title_string = argstr[len('--title='):]
+    elif argstr.startswith('--cmap='):
+        cmap = argstr[len('--cmap='):]
+    elif argstr.endswith('.h5'):
+        if h5file == None:
+            h5file = argstr
+        else:
+            print('??? extra .h5 file')
+    elif argstr == '-h' or argstr == '--help':
+        print('usage: python '+sys.argv[0] + ' qpd_data_file.h5 --title=title_string')
+        exit()
+    else:
+        print('extra argument: ' + argstr)
+
 h5file = h5py.File(sys.argv[1], "r")
 dset = h5file['/qpddataset']
 darray = dset[:]
 darray = darray[:-50,:]
 tend = darray.shape[0] * 0.01
-im = plt.imshow(darray.T / np.max(darray), origin='lower', extent=[0, tend, 2300, 2500], aspect='auto', cmap='plasma')
+im = plt.imshow(darray.T / np.max(darray), origin='lower', extent=[0, tend, 2300, 2500], aspect='auto', cmap=cmap)
 plt.xlabel('time (sec)')
 plt.ylabel('curve coeff')
-plt.title('normalized QPD magnitude\n')
+if title_string != None:
+    plt.title(title_string)
 plt.colorbar(im)
 plt.show()
